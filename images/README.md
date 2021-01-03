7eec73b5cedbf3734b9e09def84d5afc480a2c05
porcunadev <patrickgporcuna@gmail.com>
2021-01-02 16:59:52 -0800

    first commit

---
+++ images/README.md
@@ -0,0 +1,3 @@
+This application can retrieve any images on the web and turn them into jpg file. 
+
+right click, and copy url address. 
---
+++ main.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+def dl_img_to_jpg(url, file_path, file_name):
+    full_path = file_path + file_name + '.jpg'
+    urllib.request.urlretrieve(url,full_path)
+
+name = input('Hello, what is your name?')
+url = input('Enter image URL to download:')
+file_name = input('Enter file name to save as:')
+
+dl_img_to_jpg(url,'images/', file_name)
+
+print(f"Thank you {name} for using our services")
